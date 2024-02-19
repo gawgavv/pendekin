964137cb57a56cc1534e7523927664399bc7c824
gawgavv <paramartasukma@gmail.com>
2024-02-19 13:19:29 +0700

    feat: validate incoming request body on URL field

--- server/src/urls/dto/create-url.dto.ts
+++ server/src/urls/dto/create-url.dto.ts
@@ -1,4 +1,17 @@
+import { IsNotEmpty, IsUrl } from 'class-validator';
+
 export class CreateUrlDto {
 
+    @IsNotEmpty(
+        {
+            message: `Please enter the URL`
+        }
+    )
+    @IsUrl(
+        {},
+        {
+            message: `Invalid URL format`
+        }
+    )
     origin: string;
 }
