73f0e132dc9ba6804985c8c5e28bbe8db9c6a28c
gawgavv <paramartasukma@gmail.com>
2024-02-19 10:29:08 +0700

    chore: configuring environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { UrlsModule } from './urls/urls.module';
+import { ClicksModule } from './clicks/clicks.module';
+
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UrlsModule,
+    ClicksModule,
+    ConfigModule.forRoot()
+  ],
+  controllers: [
+    AppController
+  ],
   providers: [],
 })
 export class AppModule {}
